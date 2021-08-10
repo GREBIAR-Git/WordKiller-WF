915f3a942d876743bd4778cff022d4bac8dc626c
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-08-11 00:34:34 +0300

    full word

--- report/MakeReportWord/MakeReport.cs
+++ report/MakeReportWord/MakeReport.cs
@@ -7,10 +7,9 @@ namespace MakeReportWord
     {
         Document doc;
         Range word;
-
+        bool pgBreak = true;
         public void CreateReportLab(string faculty, string numberLab, string theme, string discipline, string professor, string year)
         {
-            var LengthDoc = 0;
             Application app = new Application();
             app.Visible = true;
             doc = app.Documents.Add();
@@ -22,26 +21,26 @@ namespace MakeReportWord
                 "«ОРЛОВСКИЙ ГОСУДАРСТВЕННЫЙ УНИВЕРСИТЕТ" + SkipLine(1) +
                 "ИМЕНИ И.С.ТУРГЕНЕВА»" + SkipLine(2) +
                 "Кафедра " + faculty + SkipLine(3);
-            WriteTextWord(text);
+            FirstTextDoc(text);
             PageMargin(2,2,3,1.5f);
             word.Font.Size = 14;
             word.Font.Name = "Times New Roman";
             word.Paragraphs.SpaceAfter = 0;
             word.Paragraphs.Space1();
-            word.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
 
             text = "ОТЧЁТ";
-            WriteTextWord(ref LengthDoc, text);
+            WriteTextWord(text);
             word.Font.Size = 16;
             word.Font.Bold = 1;
 
             text = "По лабораторной работе №" + numberLab;
-            WriteTextWord(ref LengthDoc, text);
+            WriteTextWord(text);
             word.Paragraphs.SpaceAfter = 10;
             word.Font.Bold = 0;
 
             text = "на тему: «" + theme + "»" + SkipLine(1) + "по дисциплине: «" + discipline + "»" + SkipLine(8); ;
-            WriteTextWord(ref LengthDoc, text);
+            WriteTextWord(text);
             word.Font.Size = 14;
             word.Paragraphs.SpaceAfter = 0;
 
@@ -49,62 +48,92 @@ namespace MakeReportWord
                 "Институт приборостроения, автоматизации и информационных технологий" + SkipLine(1) +
                 "Направление: 09.03.04 «Программная инженерия»" + SkipLine(1) +
                 "Группа: 92ПГ";
-            WriteTextWord(ref LengthDoc, text);
-            word.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
+            WriteTextWord(text);
+            word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
 
             text = "Проверил: " + professor;
-            WriteTextWord(ref LengthDoc, text);
+            WriteTextWord(text);
             word.Paragraphs.SpaceAfter = 10;
 
             text = SkipLine(1) + "Отметка о зачёте: ";
-            WriteTextWord(ref LengthDoc, text);
+            WriteTextWord(text);
             word.Paragraphs.SpaceAfter = 0;
 
             text = "Дата: «____» __________ " + year + "г.";
-            WriteTextWord(ref LengthDoc, text);
-            word.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
+            WriteTextWord(text);
+            word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphRight;
 
             text = SkipLine(8) + "Орел, " + year;
-            WriteTextWord(ref LengthDoc, text);
-            word.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+            WriteTextWord(text);
+            word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
 
-            PageBreak(ref LengthDoc);
-        }
-
-        void WriteTextWord(ref int Length, string text)
-        {
-            Length += word.Text.Length;
-            word.Text += text;
-            word = doc.Range(Length, Type.Missing);
+            PageBreak();
+            PageBreak();
+            DefaultText("Привет1");
+            Heading1("заголовок 1");
+            Heading1("заголовок 2");
+            Heading1("заголовок 3");
+            DefaultText("Привет2");
+            Heading1("заголовок 4");
         }
 
         void WriteTextWord(string text)
+        {
+            Range word1 = doc.Range();
+            int Length = word1.Text.Length;
+            try
+            {
+                word.Text += text;
+                if(pgBreak)
+                {
+                    word = doc.Range(Length, Type.Missing);
+                }
+                else
+                {
+                    word = doc.Range(Length-1, Type.Missing);
+                    pgBreak = true;
+                }
+            }
+            catch
+            {
+                word = doc.Range();
+                word.Text = text;
+            }
+        }
+        void FirstTextDoc(string text)
         {
             word = doc.Range();
             word.Text = text;
         }
 
-        void PageBreak(ref int Length)
+        void PageBreak()
         {
-            Length += word.Text.Length - 1;
-            word = doc.Range(Length, Type.Missing);
+            pgBreak = false;
+            Range word1 = doc.Range();
+            int Length = word1.Text.Length;
+            word = doc.Range(Length - 1, Type.Missing);
             word.InsertBreak(0);
+            
             word.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
         }
 
-        void DefaultText()
+        void DefaultText(string text)
         {
+            WriteTextWord(text);
+            word.Font.AllCaps = 0;
             word.Font.Size = 14;
             word.Font.Bold = 0;
             word.Font.ColorIndex = 0;
             word.Paragraphs.Space15();
             word.Paragraphs.FirstLineIndent = CentimetersToPoints(1.5f);
-            // word.Paragraphs.LeftIndent = CentimetersToPoints(1.5f);
             word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphLeft;
         }
 
-        void Heading1()
+        void Heading1(string text)
         {
+            PageBreak();
+            WriteTextWord(text);
+            word.Paragraphs.FirstLineIndent = 0;
             word.Font.Size = 14;
             word.Font.Bold = 1;
             word.Font.AllCaps = 1;
@@ -113,8 +142,11 @@ namespace MakeReportWord
             word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
         }
 
-        void Heading2()
+        void Heading2(string text)
         {
+            WriteTextWord(text);
+            word.Paragraphs.FirstLineIndent = 0;
+            word.Font.AllCaps = 0;
             word.Font.Size = 14;
             word.Font.Bold = 1;
             word.Font.ColorIndex = 0;
@@ -125,12 +157,17 @@ namespace MakeReportWord
         void Picture()
         {
             word.Paragraphs.Space15();
+            word.Paragraphs.FirstLineIndent = 0;
+            word.Font.AllCaps = 0;
             word.Font.ColorIndex = 0;
             word.Paragraphs.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
         }
 
-        void СaptionForPicture()
+        void СaptionForPicture(string text)
         {
+            WriteTextWord(text);
+            word.Paragraphs.FirstLineIndent = 0;
+            word.Font.AllCaps = 0;
             word.Font.Size = 14;
             word.Font.Bold = 0;
             word.Font.ColorIndex = 0;
