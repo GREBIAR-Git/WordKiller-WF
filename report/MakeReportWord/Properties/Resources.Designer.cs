de397d2b7efb384266a2c916aec821e707c0db63
GREMIAR <70177742+GREMIAR@users.noreply.github.com>
2021-09-08 14:48:37 +0300

    fix

--- report/MakeReportWord/Properties/Resources.Designer.cs
+++ report/MakeReportWord/Properties/Resources.Designer.cs
@@ -369,15 +369,5 @@ namespace MakeReportWord.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap white {
-            get {
-                object obj = ResourceManager.GetObject("white", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
